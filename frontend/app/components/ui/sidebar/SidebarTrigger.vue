c76f1591ba42f831946bd3f572486360d0489167
afadel151 <af8356207@gmail.com>
2026-02-27 02:18:36 +0100

    shadcn-vue components

--- frontend/app/components/ui/sidebar/SidebarTrigger.vue
+++ frontend/app/components/ui/sidebar/SidebarTrigger.vue
@@ -6,22 +6,28 @@ import { Button } from '@/components/ui/button'
 import { useSidebar } from "./utils"
 
 const props = defineProps<{
-  class?: HTMLAttributes["class"]
+  class?: HTMLAttributes["class"],
+  title?: string | unknown
 }>()
 
 const { toggleSidebar } = useSidebar()
 </script>
 
 <template>
-  <Button
+  <div class="flex items-center">
+
+    <Button
     data-sidebar="trigger"
     data-slot="sidebar-trigger"
     variant="ghost"
     size="icon"
     :class="cn('h-7 w-7', props.class)"
     @click="toggleSidebar"
-  >
+    >
     <PanelLeft />
     <span class="sr-only">Toggle Sidebar</span>
+    
   </Button>
+  <p class="ml-5 font-bold">{{ title }}</p>
+</div>
 </template>
