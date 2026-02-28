06a5b80bb3bb4476a50b0558e5c9d3432537b44f
afadel151 <af8356207@gmail.com>
2026-02-28 02:22:48 +0100

    ontology page, header with set active button

---
+++ frontend/app/pages/ontology/[id].vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import KpiCard from '~/components/KpiCard.vue'
+const route = useRoute()
+definePageMeta({
+    title: "Ontology details",
+})
+const isActive = ref(false)
+function toggleActive()  {
+    isActive.value = !isActive.value
+    console.log(isActive.value);   
+}
+</script>
+
+<template>
+    <div class="p-6 space-y-6">
+        <Card class="flex flex-row px-5  justify-between items-center">
+                <CardTitle>
+                    Ontology name (file.owl)
+                </CardTitle>
+                <Badge v-if="isActive" variant="rdf">
+                    Active
+                </Badge>
+                <Button v-else v-on:click="toggleActive()">
+                    Set as active
+                </Button>
+        </Card>
+         <div class="grid grid-cols-5 gap-4">
+            <KpiCard title="Classes" data="20" />
+            <KpiCard title="Object Properties" data="20" />
+            <KpiCard title="Data Properties" data="20" />
+            <KpiCard title="Annotations" data="20" />
+            <KpiCard title="Individuals" data="20" />
+         </div>
+    </div>
+</template>
