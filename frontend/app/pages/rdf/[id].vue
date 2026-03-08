91f66e77c8af4ddfca6266136f822b4773f740f6
Akram Fadel <af8356207@gmail.com>
2026-03-08 15:40:40 +0100

    RDF graphs and file pages

--- frontend/app/pages/rdf/[id].vue
+++ frontend/app/pages/rdf/[id].vue
@@ -5,7 +5,63 @@ import KpiCard from '~/components/KpiCard.vue'
 import ObjectsTable from '~/components/rdf/ObjectsTable.vue'
 import PredicatesTable from '~/components/rdf/PredicatesTable.vue'
 import SubjectsTable from '~/components/rdf/SubjectsTable.vue'
+import type { GraphFileStats, GraphElements } from '~/types/rdf'
+
+const config = useRuntimeConfig()
+
 const route = useRoute()
+const stats = ref<GraphFileStats>({
+    name: '',
+    format: '',
+    uploaded_at: '',
+    subjects_count: 0,
+    predicates_count: 0,
+    objects_count: 0,
+    graph_id: '',
+    file_size: 0,
+    triples_count: 0
+})
+
+
+const elements = ref<GraphElements>({
+    subjects: [],
+    predicates: [],
+    objects: []
+})
+
+
+async function fetchStats() {
+    try {
+        const { data, pending, error } = await useFetch<GraphFileStats>(`${config.public.apiBase}/rdf/${route.params.id}/stats`)
+        if (error.value) {
+            console.error('Error fetching graph stats:', error.value);
+            return;
+        }
+        if (data.value) {
+            stats.value = data.value;
+        }
+    } catch (error) {
+        console.error('Failed to fetch graph stats:', error);
+    }
+}
+async function fetchElements() {
+    try {
+        const { data, pending, error } = await useFetch<GraphElements>(`${config.public.apiBase}/rdf/${route.params.id}/elements`)
+        if (error.value) {
+            console.error('Error fetching graph elements:', error.value);
+            return;
+        }
+        if (data.value) {
+            elements.value = data.value;
+        }
+    } catch (error) {
+        console.error('Failed to fetch graph elements:', error);
+    }
+}
+onMounted(() => {
+    fetchStats();
+    fetchElements();
+});
 definePageMeta({
     title: "Graph details",
 })
@@ -14,19 +70,31 @@ function toggleActive() {
     isActive.value = !isActive.value
     console.log(isActive.value);
 }
+
+function getFormattedDate(dateTimeStr: string): string {
+    const date = new Date(dateTimeStr);
+    return date.toLocaleDateString('fr-FR'); // French format
+}
 </script>
 
 <template>
     <div class="p-2 space-y-6">
         <Card class="flex flex-row px-5  justify-between items-center">
-            <CardTitle>
-                Graph name (file.ttl)
-            </CardTitle>
+            <CardHeader class="w-fit">
+                <p class="text-2xl   ">
+                    {{ stats.name || 'undefined name' }}.{{ stats.format || 'undefined format' }}
+                </p>
+                <CardDescription class="w-fit">
+                    {{ getFormattedDate(stats.uploaded_at || 'unknown') }}
+                </CardDescription>
+            </CardHeader>
             <div class="space-x-2 flex items-center">
-                <Button size="sm" variant="outline">
-                    <Waypoints/>
-                    Visualise
-                </Button>
+                <NuxtLink :to="'/visualise/'+ stats.graph_id">
+                    <Button size="sm" variant="outline">
+                        <Waypoints />
+                        Visualise
+                    </Button>
+                </NuxtLink>
                 <Button size="sm" v-on:click="toggleActive()">
                     {{ isActive ? 'Set as Inactive' : 'Set as Active' }}
                 </Button>
@@ -39,10 +107,9 @@ function toggleActive() {
             </div>
         </Card>
         <div class="grid grid-cols-3 gap-4">
-            <KpiCard title="Subjects" data="20" />
-            <KpiCard title="Predicates" data="20" />
-
-            <KpiCard title="Objects" data="20" />
+            <KpiCard title="Subjects" :data="stats.subjects_count ?? 0" />
+            <KpiCard title="Predicates" :data="stats.predicates_count ?? 0" />
+            <KpiCard title="Objects" :data="stats.objects_count ?? 0" />
 
         </div>
         <div class="flex w-full flex-col  gap-6">
@@ -59,13 +126,13 @@ function toggleActive() {
                     </TabsTrigger>
                 </TabsList>
                 <TabsContent value="subjects">
-                    <SubjectsTable/>
+                    <SubjectsTable :subjects="elements.subjects" />
                 </TabsContent>
                 <TabsContent value="predicates">
-                    <PredicatesTable />
+                    <PredicatesTable :predicates="elements.predicates" />
                 </TabsContent>
                 <TabsContent value="objects">
-                    <ObjectsTable/>
+                    <ObjectsTable :objects="elements.objects" />
                 </TabsContent>
             </Tabs>
         </div>
--- frontend/app/pages/rdf/index.vue
+++ frontend/app/pages/rdf/index.vue
@@ -1,40 +1,80 @@
 <script setup lang="ts">
 import KpiCard from '~/components/KpiCard.vue';
 import GraphsTable from '~/components/rdf/GraphsTable.vue';
-import Badge from '~/components/ui/badge/Badge.vue';
+const config = useRuntimeConfig()
 
 
+import type { GraphsStats } from '~/types/rdf';
+const stats = ref<GraphsStats | undefined>(undefined);
 
+const files = ref<any[]>([]);   
+
+
+async function fetchStats() {
+    try {
+        const { data, pending, error } = await useFetch<GraphsStats>(`${config.public.apiBase}/rdf/stats`)
+        
+        if (error.value) {
+            console.error('Error fetching graphs stats:', error.value);
+            return;
+        }
+        stats.value = data.value;
+    } catch (error) {
+        console.error('Failed to fetch graphs stats:', error);
+    }
+}
+async function fetchFiles()
+{
+    try {
+        const { data, pending, error } = await useFetch<any[]>(`${config.public.apiBase}/rdf/files`)
+        
+        if (error.value) {
+            console.error('Error fetching graphs files:', error.value);
+            return;
+        }
+        if (data.value) {
+             files.value = data.value;
+        }
+    } catch (error) {
+        console.error('Failed to fetch graphs files:', error);
+    }
+}
+
+onMounted(() => {
+    fetchStats();
+    fetchFiles();
+});
 
 definePageMeta({
     title: "RDF Graphs",
 })
+/**
+ * {
+  "total_graphs": 1,
+  "total_triples": 59,
+  "total_subjects": 13,
+  "total_predicates": 37,
+  "total_objects": 54
+}
+ * 
+ */
+
 </script>
 
 <template>
     <div class="p-2 space-y-6">
-        <div class="grid grid-cols-4 gap-4">
-            <KpiCard title="Total graphs" data="5" />
-            <KpiCard title="Total triples" data="124" />
-            <KpiCard title="Total Size" data="50Mb" />
-            <KpiCard title="Formats used">
-                 <template #content>
+        <div class="grid grid-cols-5 gap-4">
+            <KpiCard title="Total graphs" :data="stats?.total_graphs || 0" />
+            <KpiCard title="Total triples" :data="stats?.total_triples || 0" />
+            <KpiCard title="Total subjects" :data="stats?.total_subjects || 0" />
+            <KpiCard title="Total predicates" :data="stats?.total_predicates ||0" />
+            <KpiCard title="Total objects" :data="stats?.total_objects || 0" />
 
-                     <CardContent class="space-x-2 space-y-1">
-                         <Badge>Turtle</Badge>
-                         <Badge>Turtle</Badge>
-                         <Badge>Turtle</Badge>
-                         <Badge>XML</Badge>
-                         <Badge>N-triples</Badge>
-                         <Badge>N-triples</Badge>
-                        </CardContent>
-                    </template>
-            </KpiCard>
         </div>
         <div class="w-full">
-            <GraphsTable/>
+            <GraphsTable :graphs="files" />
         </div>
-        
+
     </div>
 
 </template>
