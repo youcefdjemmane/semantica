52c0e45622cb51f574d256f016c73e3c02b3d1f7
Akram Fadel <af8356207@gmail.com>
2026-03-08 15:41:09 +0100

    edited nuxt confg

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.API_BASE_URL || 'http://localhost:8000'
+      apiBase: process.env.API_BASE_URL || 'http://localhost:8000/api/v1'
     }
   },
 
