295bd497529f9131d8c4a5de91588f5ea82f53f1
Akram Fadel <af8356207@gmail.com>
2026-03-09 12:10:10 +0100

    ontology types

---
+++ frontend/app/types/ontology.ts
@@ -0,0 +1,8 @@
+export interface OntologiesStats {
+  total_ontologies: number,
+  total_classes: number,
+  total_properties: number,
+  total_individuals: number,
+  owl_count: number,
+  rdfs_count: number
+}
